4dd2f2d07d73c5c56d05219fdde0ce50d493c6ea
MudasirSaeed032 <mudasir.saeed032@gmail.com>
2025-02-19 12:25:53 +0500

    done with the styling of the home page

---
+++ lib/start_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  StartScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
+          ),
+          SizedBox(height: 80),
+          Text(
+            'Learn Flutter the Fun Way',
+            style: TextStyle(color: Colors.white, fontSize: 25),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            icon: Icon(Icons.arrow_right_alt, color: Colors.white),
+            label: Text('Start Quiz', style: TextStyle(fontSize: 20)),
+          ),
+        ],
+      ),
+    );
+  }
+}
